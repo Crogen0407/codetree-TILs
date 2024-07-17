84b89f4ae0e38bf3b66a13c71861cc5fc59334ef
Crogen0407 <123520071+Crogen0407@users.noreply.github.com>
2024-07-17 12:16:17 +0900

    Title: 체질량지수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240717/체질량지수/bmi.cpp
+++ 240717/체질량지수/bmi.cpp
@@ -5,7 +5,7 @@ int main() {
     int h, w;
     cin >> h >> w;
     int result = (10000 * w) / (h * h);
-    cout << result;
+    cout << result << endl;
 
     if(result >= 25)
         cout << "Obesity";
