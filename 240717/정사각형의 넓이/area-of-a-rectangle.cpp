661ed9868013064d326ec366343fcca040ab2d79
Crogen0407 <123520071+Crogen0407@users.noreply.github.com>
2024-07-17 12:19:57 +0900

    Title: 정사각형의 넓이, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240717/정사각형의 넓이/area-of-a-rectangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cin << n*n < endl;
+
+    if(n<5)
+    {
+        cout << "tiny";
+    }
+    return 0;
+}
