3704ad930a6696a7e3446f80378eacb1e333363d
Crogen0407 <123520071+Crogen0407@users.noreply.github.com>
2024-07-17 12:39:25 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240717/실수 받아 그대로 출력/enter-real-value-and-print.cpp
+++ 240717/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
     double n;
     cin >> n;
-    cout << fixed << setprecision(2);
-
-
-    cout << n;
+    cout << round(100 * n) / 100;
     return 0;
 }
